ac1613926ce270a44705b7efdf471838bda39af4
Andott1 <145018255+Andott1@users.noreply.github.com>
2023-12-04 19:00:05 +0800

    Update Olaer_SmallestFactor.py

--- Olaer_SmallestFactor.py
+++ Olaer_SmallestFactor.py
@@ -1,4 +1,4 @@
-# Get smallest factor
+# Get the smallest factor
 def check_factor(n):
     # Check for divisibility from 2 to the square root of n
     for i in range(2, int(n**0.5) + 1):
@@ -11,8 +11,8 @@ def check_factor(n):
 def get_user_input():
     while True:
         try:
-            num = int(input("Enter an integer: "))
-            if num >= 2:  # Check if number is less than or greater than 2
+            num = int(input("Enter an integer greater than or equal to 2: "))
+            if num >= 2:  # Check if the number is less than or greater than 2
                 return num
             else:
                 print("Invalid input. Enter a number greater than or equal to 2.\n")
@@ -35,7 +35,7 @@ def main():
     while True:
         num = get_user_input()
         result = check_factor(num)
-        print("The smallest factor of", str(num), "(other than 1) is", str(result), "\n")
+        print("The smallest factor of", str(num), "other than 1 is", str(result), "\n")
 
         next_input = check_input("Do you want to input another integer? (yes/no): ")
         if next_input == 'yes':
